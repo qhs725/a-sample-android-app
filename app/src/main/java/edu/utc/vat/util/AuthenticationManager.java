a1061def1a1b466f6d0cb0837d2eb789a68bda80
Joshua Haynes <qhs725@mocs.utc.edu>
2015-10-18 11:31:56 -0400

    added dependencies for Oauth and Bluemix

---
+++ app/src/main/java/edu/utc/vat/util/AuthenticationManager.java
@@ -0,0 +1,15 @@
+package edu.utc.vat.util;
+
+public class AuthenticationManager 
+{
+	
+	public static String GOOGLE_AUTHENTICATION_PROVIDER = "google_auth";
+	public static String WORKLIGHT_AUTHENTICATION_PROVIDER = "wl_auth";
+	
+
+	public static void getWorklightAccountID()
+	{
+		
+	}
+
+}
