7b1933566b1edcdb5a39115f8d7fc8a0a7256e47
rossgritz <rossgritz@gmail.com>
2015-11-24 23:50:00 -0500

    -- bt framework --

---
+++ app/src/main/java/edu/utc/vat/bluetooth/BtActivity.java
@@ -0,0 +1,17 @@
+/**
+ * UTC Virt Athletic Trainer v000
+ * rg 11/24/15
+ */
+
+package edu.utc.vat.bluetooth;
+
+import android.app.Activity;
+
+
+
+
+public class BtActivity extends Activity {
+
+
+
+}
