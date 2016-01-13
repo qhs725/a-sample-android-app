867d7cf113909d223cc03b2757fb87cd0c84aee8
qhs725 <qhs725@mocs.utc.edu>
2016-01-13 17:03:06 -0500

    merged layout xml files pertaining to the Sports Form, fixed issues with Scrolling View with bigger phones

--- app/src/main/java/edu/utc/vat/forms/SportInjuryForm.java
+++ app/src/main/java/edu/utc/vat/forms/SportInjuryForm.java
@@ -229,15 +229,6 @@ public class SportInjuryForm extends AppCompatActivity {
                 }
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     //Changes displayed part 1 question to the one at the given index.
